e43eac41edc063c7df6bfc1dea0d63429af3b8dd
Timo Korhonen <tkorhonen@telia.com>
2023-03-22 09:07:15 +0100

    do not use hoverxref for pvxs, at least for now.

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,6 @@ hoverxref_intersphinx = [
     'how-tos',
     'epics',
     'mydocs',
-    'pvxs'
 ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
